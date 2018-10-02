52984b49d353097790946fb402694476773fd5a6
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-10-02 17:44:13 +0800

    mod: AppMain

--- src/views/layout/components/AppMain.vue
+++ src/views/layout/components/AppMain.vue
@@ -1,8 +1,12 @@
 <template>
   <section>
-    <transition name="fade-transform" mode="out-in">
-      <router-view/>
-    </transition>
+    <v-content>
+      <v-container fluid>
+        <v-fade-transition mode="out-in">
+          <router-view/>
+        </v-fade-transition>
+      </v-container>
+    </v-content>
   </section>
 </template>
 
