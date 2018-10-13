3b8a4640a46e8e7dae1bd0eccf6ee3789f0deee4
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-10-14 02:01:12 +0800

    add: `layoutWithoutSidebar` component

---
+++ src/views/layouts/LayoutWithoutSidebar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app class="app-wrapper">
+    <navbar class="nav-container"/>
+    <app-main class="main-container"/>
+  </v-app>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  name: 'LayoutWithoutSidebar',
+  components: {
+    Navbar,
+    AppMain,
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.app-wrapper {
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+</style>
