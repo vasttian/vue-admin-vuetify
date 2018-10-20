8d781df6ff4b1ec16064aad0b0650b8a155c1086
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-10-20 21:03:28 +0800

    mod: LangBar component

--- src/components/widgets/LangBar.vue
+++ src/components/widgets/LangBar.vue
@@ -29,3 +29,21 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.change-lang {
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
+
+  span {
+    cursor: pointer;
+    padding: 0 2px;
+    color: #c0c4cc;
+
+    &.active-lang {
+      color: #0302f5;
+    }
+  }
+}
+</style>
