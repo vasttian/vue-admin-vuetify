16d5689614cd0f070d2027126ba852489692e949
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-10-21 16:51:13 +0800

    dnf: sql editor

--- src/views/components/SqlEditor.vue
+++ src/views/components/SqlEditor.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
-    SqlEditor
+    <sql-editor
+      ref="sqlEditor"
+      v-model="sqlText"
+      placeholder=""/>
   </div>
 </template>
+
+<script>
+import SqlEditor from '@/components/editors/SqlEditor.vue';
+
+export default {
+  name: 'SqlEditorComponent',
+  components: {
+    SqlEditor,
+  },
+  data() {
+    return {
+      sqlText: 'select user from users;',
+    };
+  },
+  methods:{},
+  creatd() {
+
+  },
+}
+</script>
+
