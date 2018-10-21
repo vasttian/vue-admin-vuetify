afc2c0eba90c8d01c3165e3bbc2780c177f872b2
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-10-21 20:21:32 +0800

    feat: add JSON editor

--- src/views/components/JsonEditor.vue
+++ src/views/components/JsonEditor.vue
@@ -1,5 +1,49 @@
 <template>
   <div>
-    JsonEditor
+    <json-editor
+      v-model="jsonText"
+      placeholder=""/>
   </div>
 </template>
+
+<script>
+import JsonEditor from '@/components/editors/JsonEditor.vue';
+
+export default {
+  name: 'JsonEditorComponent',
+  components: {
+    JsonEditor,
+  },
+  data() {
+    return {
+      jsonText: {
+        $schema: 'http://json-schema.org/draft-04/schema#',
+        title: 'Product',
+        description: "A product from Acme's catalog",
+        type: 'object',
+        properties: {
+          id: {
+            description: 'The unique identifier for a product',
+            type: 'integer',
+          },
+          name: {
+            description: 'Name of the product',
+            type: 'string',
+          },
+          price: {
+            type: 'number',
+            minimum: 0,
+            exclusiveMinimum: true,
+          },
+        },
+        required: [
+          'id',
+          'name',
+          'price',
+        ],
+      },
+    };
+  },
+};
+</script>
+
