f9bbef491ca0b4581e0e4d90335e7a6b2fc213c5
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-10-27 02:18:50 +0800

    mod: baidu map

--- src/components/widgets/BaseBaiduMap.vue
+++ src/components/widgets/BaseBaiduMap.vue
@@ -1,11 +1,37 @@
 <template>
-  <baidu-map
-    class="bm-view"
-    :center="center"
-    :zoom="zoom"
-    @ready="handler"
-    :ak="appKey">
-  </baidu-map>
+  <div>
+    <v-form>
+      <v-container>
+        <v-layout row wrap>
+          <v-flex xs8 sm4>
+            <v-text-field
+              label="Longitude"
+              v-model.number="center.lng"
+              prepend-icon="place"
+            ></v-text-field>
+            <v-text-field
+              label="Latitude"
+              v-model.number="center.lat"
+              prepend-icon="place"
+            ></v-text-field>
+            <v-text-field
+              label="Zoom"
+              v-model.number="zoom"
+              prepend-icon="zoom_out_map"
+            ></v-text-field>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-form>
+    <baidu-map
+      class="bm-view"
+      :center="center"
+      :zoom="zoom"
+      :scroll-wheel-zoom="true"
+      @ready="handler"
+      :ak="appKey">
+    </baidu-map>
+  </div>
 </template>
 
 <script>
@@ -20,10 +46,11 @@ export default {
     return {
       appKey: 'np5hzZH52fbM7H1k1TVh8Gwln4GV4ldv',
       center: {
-        lng: 0,
-        lat: 0,
+        lng: 116.404,
+        lat: 39.915,
       },
       zoom: 3,
+      mapType: 'BMAP_SATELLITE_MAP',
     };
   },
   methods: {
@@ -31,7 +58,7 @@ export default {
       console.log(BMap, map);
       // this.center.lng = 116.404;
       // this.center.lat = 39.915;
-      this.center = '深圳';
+      // this.center = '深圳';
       this.zoom = 13;
     },
   },
