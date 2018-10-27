539d77c60b22bef9ca8b523bfd4413c582f7b62b
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-10-27 22:46:54 +0800

    mod: baidu map

--- src/components/widgets/BaseBaiduMap.vue
+++ src/components/widgets/BaseBaiduMap.vue
@@ -3,17 +3,21 @@
     <v-form>
       <v-container>
         <v-layout row wrap>
-          <v-flex xs8 sm4>
+          <v-flex xs12 sm4>
             <v-text-field
               label="Longitude"
               v-model.number="center.lng"
               prepend-icon="place"
             ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm4>
             <v-text-field
               label="Latitude"
               v-model.number="center.lat"
               prepend-icon="place"
             ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm4>
             <v-text-field
               label="Zoom"
               v-model.number="zoom"
@@ -29,6 +33,9 @@
       :zoom="zoom"
       :scroll-wheel-zoom="true"
       @ready="handler"
+      @moving="syncCenterAndZoom"
+      @moveend="syncCenterAndZoom"
+      @zoomend="syncCenterAndZoom"
       :ak="appKey">
     </baidu-map>
   </div>
@@ -46,20 +53,26 @@ export default {
     return {
       appKey: 'np5hzZH52fbM7H1k1TVh8Gwln4GV4ldv',
       center: {
-        lng: 116.404,
-        lat: 39.915,
+        lng: 113.952,
+        lat: 22.543,
       },
-      zoom: 3,
+      zoom: 15,
       mapType: 'BMAP_SATELLITE_MAP',
     };
   },
   methods: {
+    syncCenterAndZoom (e) {
+      const { lng, lat } = e.target.getCenter();
+      this.center.lng = lng;
+      this.center.lat = lat;
+      this.zoom = e.target.getZoom();
+    },
     handler({ BMap, map }) {
-      console.log(BMap, map);
+      // console.log(BMap, map);
       // this.center.lng = 116.404;
       // this.center.lat = 39.915;
       // this.center = '深圳';
-      this.zoom = 13;
+      // this.zoom = 13;
     },
   },
 };
