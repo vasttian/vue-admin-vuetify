287297fcfbdb2bd16ff6f6b568a86edab75de295
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-10-28 00:21:19 +0800

    add: `BmCityList`, `BmNavigation`, ` BmMapType`, `BmOverviewMap`, `BmGeolocation` components

--- src/components/widgets/BaseBaiduMap.vue
+++ src/components/widgets/BaseBaiduMap.vue
@@ -37,17 +37,47 @@
       @moveend="syncCenterAndZoom"
       @zoomend="syncCenterAndZoom"
       :ak="appKey">
+      <bm-city-list
+        :offset="{ width: 10, height: 40 }"
+        anchor="BMAP_ANCHOR_TOP_LEFT"></bm-city-list>
+      <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+      <bm-map-type
+        :map-types="['BMAP_NORMAL_MAP', 'BMAP_HYBRID_MAP']"
+        :offset="{ width: 10, height: 10 }"
+        anchor="BMAP_ANCHOR_TOP_LEFT"></bm-map-type>
+      <bm-overview-map
+        anchor="BMAP_ANCHOR_BOTTOM_RIGHT"
+        :isOpen="true"></bm-overview-map>
+      <bm-geolocation
+        anchor="BMAP_ANCHOR_BOTTOM_RIGHT"
+        :showAddressBar="true"
+        :autoLocation="true"></bm-geolocation>
+      <!-- <bm-marker :position="{lng: 116.404, lat: 39.915}" :dragging="true" animation="BMAP_ANIMATION_BOUNCE">
+        <bm-label content="我爱北京天安门" :labelStyle="{color: 'red', fontSize : '24px'}" :offset="{width: -35, height: 30}"/>
+      </bm-marker> -->
     </baidu-map>
   </div>
 </template>
 
 <script>
 import BaiduMap from 'vue-baidu-map/components/map/Map.vue';
+import {
+  BmNavigation,
+  BmMapType,
+  BmOverviewMap,
+  BmGeolocation,
+  BmCityList,
+} from 'vue-baidu-map/components';
 
 export default {
   name: 'BaseBaiduMap',
   components: {
     BaiduMap,
+    BmCityList,
+    BmNavigation,
+    BmMapType,
+    BmOverviewMap,
+    BmGeolocation,
   },
   data() {
     return {
