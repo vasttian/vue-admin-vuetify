3140be0db70c05320f0f2a83b016e44e27d54a9a
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-10-29 14:06:10 +0800

    mod: default JSON

--- src/views/components/JsonEditor.vue
+++ src/views/components/JsonEditor.vue
@@ -16,32 +16,79 @@ export default {
   },
   data() {
     return {
-      jsonText: {
-        $schema: 'http://json-schema.org/draft-04/schema#',
-        title: 'Product',
-        description: "A product from Acme's catalog",
-        type: 'object',
-        properties: {
-          id: {
-            description: 'The unique identifier for a product',
-            type: 'integer',
-          },
-          name: {
-            description: 'Name of the product',
-            type: 'string',
-          },
-          price: {
-            type: 'number',
-            minimum: 0,
-            exclusiveMinimum: true,
-          },
+      /* eslint-disable */
+      jsonText: [
+        {
+          "description": "simple enum validation",
+          "schema": {"enum": [1, 2, 3]},
+          "tests": [
+            {
+              "description": "one of the enum is valid",
+              "data": 1,
+              "valid": true
+            },
+            {
+              "description": "something else is invalid",
+              "data": 4,
+              "valid": false
+            }
+          ]
         },
-        required: [
-          'id',
-          'name',
-          'price',
-        ],
-      },
+        {
+          "description": "heterogeneous enum validation",
+          "schema": {"enum": [6, "foo", [], true, {"foo": 12}]},
+          "tests": [
+            {
+              "description": "one of the enum is valid",
+              "data": [],
+              "valid": true
+            },
+            {
+              "description": "something else is invalid",
+              "data": null,
+              "valid": false
+            },
+            {
+              "description": "objects are deep compared",
+              "data": {"foo": false},
+              "valid": false
+            }
+          ]
+        },
+        {
+          "description": "enums in properties",
+          "schema": {
+            "type":"object",
+            "properties": {
+              "foo": {"enum":["foo"]},
+              "bar": {"enum":["bar"]}
+            },
+            "required": ["bar"]
+          },
+          "tests": [
+            {
+              "description": "both properties are valid",
+              "data": {"foo":"foo", "bar":"bar"},
+              "valid": true
+            },
+            {
+              "description": "missing optional property is valid",
+              "data": {"bar":"bar"},
+              "valid": true
+            },
+            {
+              "description": "missing required property is invalid",
+              "data": {"foo":"foo"},
+              "valid": false
+            },
+            {
+              "description": "missing all properties is invalid",
+              "data": {},
+              "valid": false
+            }
+          ]
+        }
+      ],
     };
   },
 };
