6a46587f65303a9cf097b5364f9cd6871bb1635a
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-11-04 00:10:29 +0800

    add: `VBasicCard` component

---
+++ src/components/VBasicCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card>
+    <v-toolbar
+      v-if="fillHeader"
+      flat
+      dense
+      color="transparent"
+      :height="toolbarHeight"
+      card>
+      <v-toolbar-title>
+        <v-subheader class="pl-0">
+          {{ title }}
+        </v-subheader>
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-divider v-if="fillHeader"></v-divider>
+    <v-card-text>
+      <slot name="card-content"></slot>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'VBasicCard',
+  props: {
+    fillHeader: {
+      type: Boolean,
+      default: true,
+    },
+    toolbarHeight: {
+      type: [Number, String],
+      default: undefined,
+    },
+    title: {
+      type: String,
+    },
+  },
+};
+</script>
+
