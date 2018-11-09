c982205c6938b2faebf2d605a7096e7838465404
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-11-09 10:48:46 +0800

    add: tinymce editor

--- src/views/components/TinymceEditor.vue
+++ src/views/components/TinymceEditor.vue
@@ -1,17 +1,75 @@
 <template>
-  <div>TinymceEditor</div>
+  <div>
+    <vue-tinymce
+      ref="tinymce"
+      :init="editorInit"
+      v-model="content"/>
+  </div>
 </template>
 
 <script>
-import TinymceEditor from '@tinymce/tinymce-vue';
+import tinymce from 'tinymce/tinymce';
+import 'tinymce/themes/modern/theme';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/media';
+import 'tinymce/plugins/contextmenu';
+import 'tinymce/plugins/wordcount';
+import 'tinymce/plugins/colorpicker';
+import 'tinymce/plugins/textcolor';
+import 'tinymce/plugins/preview';
+import 'tinymce/plugins/fullscreen';
+// import 'tinymce/plugins/emoticons';
+import VueTinymce from '@tinymce/tinymce-vue';
 
 export default {
   name: 'TinymceEditor',
   components: {
-    TinymceEditor,
+    VueTinymce,
   },
   data() {
-    return {};
+    return {
+      editorInit: {
+        height: 300,
+        language: 'zh_CN',
+        language_url: '/tinymce/zh_CN.js',
+        skin_url: '/tinymce/skins/lightgray',
+        plugins: 'textcolor table code colorpicker link lists image media wordcount contextmenu preview fullscreen',
+        toolbar: `fontsizeselect | bold italic underline strikethrough forecolor backcolor | alignleft
+          aligncenter alignright alignjustify outdent indent bullist numlist
+          blockquote | link unlink image media code | undo redo | removeformat preview fullscreen`,
+        images_upload_base_path: '/api/images',
+        images_upload_url: '',
+        images_upload_handler(blobInfo, success, failure) {
+          this.uploadImage(blobInfo, success, failure);
+        },
+      },
+      content:
+        `<h1 style="text-align: center;">Welcome to the TinyMCE demo!</h1><p style="text-align: center; font-size: 15px;"><img title="TinyMCE Logo" src="//www.tinymce.com/images/glyph-tinymce@2x.png" alt="TinyMCE Logo" width="110" height="97" /><ul>
+          <li>Our <a href="//www.tinymce.com/docs/">documentation</a> is a great resource for learning how to configure TinyMCE.</li><li>Have a specific question? Visit the <a href="https://community.tinymce.com/forum/">Community Forum</a>.</li><li>We also offer enterprise grade support as part of <a href="https://tinymce.com/pricing">TinyMCE premium subscriptions</a>.</li>
+        </ul>`,
+    };
+  },
+  methods: {
+    uploadImage(blobInfo, success, failure) {
+      const formdata = new FormData();
+      formdata.set('upload_file', blobInfo.blob());
+      this.$http.post('/upload', formdata).then((res) => {
+        const { data } = res.data;
+        this.$message.success(data.src);
+        success('Uploaded successfully');
+      }).catch((res) => {
+        console.log('tinymce-uploadImage', res);
+        this.$message.error('Failed');
+        failure('Failed');
+      });
+    },
+  },
+  mounted() {
+    tinymce.init({});
   },
 };
 </script>
