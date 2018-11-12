3a506071a070e7896a84dc1a7ae6e221bd8b2c2a
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-11-12 19:41:26 +0800

    add: Badges - Visibility

--- src/views/ui-components/VBadges.vue
+++ src/views/ui-components/VBadges.vue
@@ -25,7 +25,9 @@
               </v-badge>
             </v-flex>
 
-            <v-flex class="text-xs-center">
+            <v-flex
+              class="text-xs-center"
+              style="margin-top: 40px;">
               <v-badge
                 color="purple"
                 left
@@ -60,6 +62,60 @@
           </template>
         </v-basic-card>
       </v-flex>
+
+      <!-- Base -->
+      <v-flex xs12 sm6>
+        <v-basic-card title="Badges - Visibility">
+          <template slot="card-content">
+            <v-layout
+              justify-space-between
+              class="text-xs-center"
+              row
+              wrap>
+              <v-flex xs12>
+                <v-btn
+                  color="indigo"
+                  class="white--text"
+                  @click="show = !show">
+                  Toggle
+                </v-btn>
+              </v-flex>
+
+              <v-flex
+                xs12
+                style="margin-top: 48px;">
+                <v-badge
+                  v-model="show"
+                  color="cyan"
+                  left>
+                  <span slot="badge">6</span>
+                  <v-icon
+                    large
+                    color="grey lighten-1"
+                  >shopping_cart</v-icon>
+                </v-badge>
+
+                <v-badge
+                  v-model="show"
+                  color="purple">
+                  <span slot="badge">6</span>
+                  <v-icon large color="grey">mail</v-icon>
+                </v-badge>
+              </v-flex>
+            </v-layout>
+          </template>
+        </v-basic-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
