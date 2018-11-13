70075abf73f7c190e3bd7d9b3a7b5501e6a5e7dc
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-11-13 20:11:13 +0800

    dnf: dnd list

--- src/views/components/ListDrag.vue
+++ src/views/components/ListDrag.vue
@@ -5,7 +5,14 @@
       <v-flex xs12 sm6>
         <v-basic-card title="Drag List - Base">
           <template slot="card-content">
-            <draggable v-model="items">
+            <v-btn
+              @click="disable = !disable"
+              color="light-blue accent-2">
+              {{ disable ? 'Disable' : 'Enable' }}
+            </v-btn>
+            <draggable
+              :options="baseDragOptions"
+              v-model="items">
               <transition-group name="list-drag">
                 <div
                   v-for="(item, index) in items"
@@ -52,6 +59,58 @@
           </template>
         </v-basic-card>
       </v-flex>
+
+      <!-- group -->
+      <v-flex md6>
+        <v-basic-card title="Drag List - Group">
+          <template slot="card-content">
+            <draggable
+              :options="groupDragOptions"
+              v-model="list1"
+              element="ul">
+              <transition-group name="list-drag-group1">
+                <div
+                  v-for="item in list1"
+                  :key="item.name"
+                  class="list-drag-item">
+                  <v-layout
+                    fill-height
+                    align-center
+                    justify-space-between
+                    class="content">
+                    <span>{{ item.name }}</span>
+                  </v-layout>
+                </div>
+              </transition-group>
+            </draggable>
+          </template>
+        </v-basic-card>
+      </v-flex>
+      <v-flex md6>
+        <v-basic-card title="Drag List - Group2">
+          <template slot="card-content">
+            <draggable
+              :options="groupDragOptions"
+              v-model="list2"
+              element="ul">
+              <transition-group name="list-drag-group1">
+                <div
+                  v-for="item in list2"
+                  :key="item.name"
+                  class="list-drag-item">
+                  <v-layout
+                    fill-height
+                    align-center
+                    justify-space-between
+                    class="content">
+                    <span>{{ item.name }}</span>
+                  </v-layout>
+                </div>
+              </transition-group>
+            </draggable>
+          </template>
+        </v-basic-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -83,7 +142,13 @@ export default {
           name: 'Doris',
         },
         {
-          name: 'Eudora',
+          name: 'Emma',
+        },
+        {
+          name: 'Grace',
+        },
+        {
+          name: 'Sarah',
         },
       ],
       images: [
@@ -115,11 +180,73 @@ export default {
           src: 'http://67.218.155.2:8082/image20.jpg',
         },
       ],
+      list1: [
+        {
+          name: 'Jamie',
+        },
+        {
+          name: 'Jane',
+        },
+        {
+          name: 'Janice',
+        },
+        {
+          name: 'Jean',
+        },
+        {
+          name: 'Jenny',
+        },
+        {
+          name: 'Jessie',
+        },
+      ],
+      list2: [
+        {
+          name: 'Karen',
+        },
+        {
+          name: 'Katherine',
+        },
+        {
+          name: 'Kelly',
+        },
+        {
+          name: 'Kimberley',
+        },
+        {
+          name: 'Kitty',
+        },
+        {
+          name: 'Kristin',
+        },
+      ],
+      disable: false,
     };
+  },
+  computed: {
+    baseDragOptions() {
+      return {
+        group: 'baseDrag',
+        animation: 150,
+        disabled: this.disable,
+      };
+    },
+    groupDragOptions() {
+      return {
+        group: 'groupDemo',
+      };
+    },
   },
   methods: {
     removeItem(index) {
       this.items.splice(index, 1);
+    },
+    onMove({ relatedContext, draggedContext }) {
+      const relatedElement = relatedContext.element;
+      const draggedElement = draggedContext.element;
+      return (
+        (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
+      );
     },
   },
 };
@@ -127,6 +254,8 @@ export default {
 
 <style lang="scss">
 .list-drag-item {
+  // cursor: move;
+  cursor: -webkit-grabbing;
   margin-top: 10px;
   border: solid 1px;
   transition: all 1s;
