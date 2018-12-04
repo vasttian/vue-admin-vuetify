1c90eef13ecb70f18613609b66c87653b5ffd920
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-12-04 18:57:24 +0800

    mod: Bottom navigation

--- src/views/ui-components/VBottomNavigation.vue
+++ src/views/ui-components/VBottomNavigation.vue
@@ -2,7 +2,7 @@
   <v-container grid-list-lg pa-0>
     <v-layout wrap>
       <!-- Base -->
-      <v-flex xs12 sm6>
+      <v-flex sm12 md6>
         <v-basic-card title="BottomNavigation - Usage">
           <template slot="card-content">
             <v-card
@@ -51,6 +51,45 @@
           </template>
         </v-basic-card>
       </v-flex>
+
+      <v-flex sm12 md6>
+        <v-basic-card title="BottomNavigation - Color & shift">
+          <template slot="card-content">
+            <v-card
+              class="overflow-hidden"
+              height="300px"
+              flat>
+              <div
+                class="headline text-xs-center"
+                :style="{ color: color }">
+                Active: {{ tab }}
+              </div>
+              <v-bottom-nav
+                absolute
+                :active.sync="tab"
+                :color="color"
+                :value="true">
+                <v-btn dark>
+                  <span>Video</span>
+                  <v-icon>ondemand_video</v-icon>
+                </v-btn>
+                <v-btn dark>
+                  <span>Music</span>
+                  <v-icon>music_note</v-icon>
+                </v-btn>
+                <v-btn dark>
+                  <span>Book</span>
+                  <v-icon>book</v-icon>
+                </v-btn>
+                <v-btn dark>
+                  <span>Image</span>
+                  <v-icon>image</v-icon>
+                </v-btn>
+              </v-bottom-nav>
+            </v-card>
+          </template>
+        </v-basic-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -62,7 +101,19 @@ export default {
     return {
       showNav: true,
       bottomNav: 'recent',
+      tab: null,
     };
+  },
+  computed: {
+    color() {
+      switch (this.tab) {
+        case 0: return 'indigo';
+        case 1: return 'teal';
+        case 2: return 'blue';
+        case 3: return 'brown';
+        default: return 'purple';
+      }
+    },
   },
 };
 </script>
