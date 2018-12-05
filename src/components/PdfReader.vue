feb6096293c162192856e54b63a5639c51ea86de
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-12-05 16:25:39 +0800

    save: pdf reader

--- src/components/PdfReader.vue
+++ src/components/PdfReader.vue
@@ -1,3 +1,33 @@
 <template>
-  <div>PdfReader</div>
+  <div style="width:100%; height:100%;">
+    <iframe
+      :src="url"
+      width="100%"
+      height="100%"
+      scrolling="no">
+      {{ $t('components.notSupportPDF') }}
+    </iframe>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'PdfReader',
+  data() {
+    return {
+      url: '/pdf/web/viewer.html?file=/pdf/files/microservices-on-aws.pdf',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.iframe-wrapper {
+  iframe {
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+}
+</style>
