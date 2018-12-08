6862f26164698399e371265975b69ce452f4c416
tianyiming26@gmail.com <tianyiming26@gmail.com>
2018-12-09 01:11:36 +0800

    save: dnf

---
+++ src/views/charts/bars/VIndex.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <base-line-chart/>
+  </div>
+</template>
+
+<script>
+import BaseLineChart from './charts/BaseBarChart.vue';
+
+export default {
+  name: 'VLineIndex',
+  componets: {
+    BaseLineChart,
+  },
+};
+</script>
+
--- src/views/charts/bars/charts/BaseBarChart.vue
+++ src/views/charts/bars/charts/BaseBarChart.vue
@@ -12,3 +12,12 @@
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {
+  name: 'BaseBarChart',
+  data() {
+    return {};
+  },
+};
+</script>
